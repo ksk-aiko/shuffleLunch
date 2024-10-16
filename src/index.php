0a8ec1ac47d90ae8645a67112509880d03c0857d
愛甲啓介 <step.up.to.brighter03@gmail.com name = ksk-aiko>
2024-10-16 16:56:10 +0900

    When accessing the top page, the index action of ShuffleController is called.

--- src/index.php
+++ src/index.php
@@ -3,4 +3,20 @@
 $title = "シャッフルランチサービス";
 // viewsのindex.phpを読み込む
 require_once('views/index.php');
-?>
+// mysqli使用して、データベースに接続
+$link = mysqli_connect('db', 'test_user', 'pass', 'test_database');
+if (mysqli_connect_errno()) {
+    echo 'データベースに接続できませんでした';
+}
+// データベースから社員情報を取得
+$query = 'SELECT * FROM employees';
+if ($result = mysqli_query($link, $query)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td>';
+        echo '<td>' . $row['name'] . '</td>';
+        echo '</tr>';
+    }
+    mysqli_free_result($result);
+}
+mysqli_close($link);
--- src/views/index.php
+++ src/views/index.php
@@ -1,4 +1,3 @@
-<!-- TODO:viewとlogicを分離し、画面を描画できるようにする -->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -18,24 +17,5 @@
             <th>社員ID</th>
             <th>社員名</th>
         </tr>
-        <?php
-        // mysqli使用して、データベースに接続
-        $link = mysqli_connect('db', 'test_user', 'pass', 'test_database');
-        if (mysqli_connect_errno()) {
-            echo 'データベースに接続できませんでした';
-        }
-        // データベースから社員情報を取得
-        $query = 'SELECT * FROM employees';
-        if ($result = mysqli_query($link, $query)) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td>' . $row['id'] . '</td>';
-                echo '<td>' . $row['name'] . '</td>';
-                echo '</tr>';
-            }
-            mysqli_free_result($result);
-        }
-        mysqli_close($link);
-        ?>
 </body>
 </html>
