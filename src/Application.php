e3d8e20afa2c853c2cf2be58cb565f13b86124ef
ksk-aiko <step.up.to.brighter03@gmail.com>
2024-12-11 19:00:29 +0900

    Implement 404 error handling with HttpNotFoundException and render a custom 404 page

--- src/Application.php
+++ src/Application.php
@@ -3,23 +3,32 @@
 require_once __DIR__ . '/core/Router.php';
 require_once __DIR__ . '/controller/ShuffleController.php';
 require_once __DIR__ . '/controller/EmployeeController.php';
+require_once __DIR__ . '/core/HttpNotFoundException.php';
 
 class Application
 
 
 {
     private $router;
-    
-    public function __construct() {
-        $this->router = new Router($this -> registerRoutes());
+
+    public function __construct()
+    {
+        $this->router = new Router($this->registerRoutes());
     }
 
     public function run()
     {
-        $params = $this->router->resolve($this->getPathInfo());
-        $controller = $params['controller'];
-        $action = $params['action'];
-        $this->runAction($controller, $action);
+        try {
+            $params = $this->router->resolve($this->getPathInfo());
+            if (! $params) {
+                throw new HttpNotFoundException();
+            }
+            $controller = $params['controller'];
+            $action = $params['action'];
+            $this->runAction($controller, $action);
+        } catch (HttpNotFoundException) {
+            $this->render404Page();
+        }
     }
 
     private function runAction($controllerName, $action)
@@ -42,5 +51,24 @@ class Application
     private function getPathInfo()
     {
         return $_SERVER['REQUEST_URI'];
+    }
+
+    private function render404Page()
+    {
+        header('HTTP/1.0 404 Not Found');
+        $content = <<<EOT
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>404 Not Found</title>
+            </head>
+            <body>
+                <h1>404 Not Found</h1>
+                <p>The requested URL {$_SERVER['REQUEST_URI']} was not found on this server.</p>
+            </body>
+            </html>
+        EOT;
+        
+        echo $content;
     }
 }
--- src/core/HttpNotFoundException.php
+++ src/core/HttpNotFoundException.php
@@ -4,5 +4,3 @@ class HttpNotFoundException extends Exception
 {
 
 };
-
-//  TODO:runActionメソッドの実行時に、paramsが存在しない場合にHttpNotFoundExceptionをスローするように修正する。それをキャッチした際に、404エラーを表示するようにする。
