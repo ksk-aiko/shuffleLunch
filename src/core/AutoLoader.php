8ad72cffa0b7c2043dc920f41f117277f75b1d10
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-02-07 15:47:27 +0900

    Add TODO comments for AutoLoader instance creation and method implementations

--- src/core/AutoLoader.php
+++ src/core/AutoLoader.php
@@ -2,5 +2,11 @@
 //  TODO: Create an AutoLoader class that will be responsible for loading classes automatically.Functions that execute the built-in autoload function, functions that register directories, functions that define detailed paths, and creating the necessary properties for these
 class AutoLoader
 {
-    
+    // create a property to store the directory
+
+    // create a function to execute the built-in autoload function
+
+    // create a function to register the directory
+
+    // create a function to define the detailed path
 }
