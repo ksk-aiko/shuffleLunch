390f420bc9d2271ce566b2f1270d637ac4440ae1
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-02-07 15:55:15 +0900

    Implement AutoLoader class methods for class loading and directory registration

--- src/core/AutoLoader.php
+++ src/core/AutoLoader.php
@@ -1,12 +1,30 @@
 <?php
-//  TODO: Create an AutoLoader class that will be responsible for loading classes automatically.Functions that execute the built-in autoload function, functions that register directories, functions that define detailed paths, and creating the necessary properties for these
 class AutoLoader
 {
     // create a property to store the directory
+    private $dirs;
 
     // create a function to execute the built-in autoload function
+    public function register()
+    {
+        spl_autoload_register([$this, 'loadClass']);
+    }
 
     // create a function to register the directory
+    public function registerDir($dir)
+    {
+        $this->dirs[] = $dir;
+    }
 
     // create a function to define the detailed path
+    public function loadClass($className)
+    {
+        foreach ($this->dirs as $dir) {
+            $file = $dir . '/' . $className . '.php';
+            if (is_readable($file)) {
+                require $file;
+                return;
+            }
+        }
+    }
 }
